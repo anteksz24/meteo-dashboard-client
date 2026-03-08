033bef1f92f69d6e10180421a92d60eb8c7e9b09
anteksz24 <szantek2004@gmail.com>
2026-03-08 16:13:19 +0100

    Add DataParser class and parse_data() method returning data as dictionary

--- main.py
+++ main.py
@@ -2,12 +2,23 @@ import time
 import requests
 import sys
 import os
+import re
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
 
 file_path = sys.argv[1]
 endpoint = sys.argv[2]
 
+class DataParser:
+    def parse_data(self, data):
+        data_split = (re.split(r':|;', data[1:-1]))
+        data_dict = {}
+        for i in range(0, len(data_split), 2):
+            key = data_split[i]
+            value = data_split[i+1]
+            data_dict[key] = value
+        return data_dict
+    
 class DataSender:
     def send_request(self, data):
         print(f"Sending data to: {endpoint}")
@@ -21,18 +32,19 @@ class FileModificationHandler(FileSystemEventHandler):
     def __init__(self):
         self.last_data = ""
         self.sender = DataSender()
+        self.parser = DataParser()
 
     def on_modified(self, event):
         if event.event_type == "modified" and event.src_path == file_path:
             with open(file_path, "r") as file:
                 data = {
-                    "content": file.read(),
+                    "content": self.parser.parse_data(file.read()),
                     "password": os.getenv("METEO_PASSWORD")
                 }
                 if data["content"] != self.last_data:
                     self.last_data = data["content"]
                     self.sender.send_request(data)
-                    
+
 if __name__ == "__main__":
     file_modification_handler = FileModificationHandler()
     observer = Observer()
