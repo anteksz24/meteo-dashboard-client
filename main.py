ed3a629a97b9500be890f0de4b94ec2182c31130
anteksz24 <szantek2004@gmail.com>
2026-03-08 13:48:35 +0100

    Send POST request only when file content changes

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ file_path = sys.argv[1]
 endpoint = sys.argv[2]
 
 class FileModificationHandler(FileSystemEventHandler):
+    def __init__(self):
+        self.last_data = ""
+
     def on_modified(self, event):
         if event.event_type == "modified" and event.src_path == file_path:
             with open(file_path, "r") as file:
@@ -16,14 +19,14 @@ class FileModificationHandler(FileSystemEventHandler):
                     "content": file.read(),
                     "password": os.getenv("METEO_PASSWORD")
                 }
-                print(f"Sending data to: {endpoint}")
-                try:
-                    response = requests.post(endpoint, json = data)
-                    print(f"Status: {response.status_code}")
-                except Exception as e:
-                    print(f"Error while sending data: {e}")
-        else:
-            return
+                if data["content"] != self.last_data:
+                    self.last_data = data["content"]
+                    print(f"Sending data to: {endpoint}")
+                    try:
+                        response = requests.post(endpoint, json = data)
+                        print(f"Status: {response.status_code}")
+                    except Exception as e:
+                        print(f"Error while sending data: {e}")
 
 if __name__ == "__main__":
     file_modification_handler = FileModificationHandler()
